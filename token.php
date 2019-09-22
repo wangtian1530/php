7dbfa124fa513826f8cf90c0bffbd5831f644a18
王天 <38849263+wangtian1530@users.noreply.github.com>
2019-09-22 17:06:21 +0800

    Add files via upload

---
+++ token.php
@@ -0,0 +1,35 @@
+<?php
+function token($qtd){ 
+//Under the string $Caracteres you write all the characters you want to be used to randomly generate the code. 
+$Caracteres = 'abcdrfgjijklmopqrstuvxwyzABCDEFGHIJKLMOPQRSTUVXWYZ0123456789'; 
+$QuantidadeCaracteres = strlen($Caracteres); 
+$QuantidadeCaracteres--; 
+
+$Hash=NULL; 
+    for($x=1;$x<=$qtd;$x++){ 
+        $Posicao = rand(0,$QuantidadeCaracteres); 
+        $Hash .= substr($Caracteres,$Posicao,1); 
+    } 
+
+return $Hash; 
+} 
+
+//Here you specify how many characters the returning string must have 
+function id($qtdy){ 
+//Under the string $Caracteres you write all the characters you want to be used to randomly generate the code. 
+$Caracteres = '1234567890'; 
+$QuantidadeCaracteres = strlen($Caracteres); 
+$QuantidadeCaracteres--; 
+
+$Hash=NULL; 
+    for($x=1;$x<=$qtdy;$x++){ 
+        $Posicao = rand(0,$QuantidadeCaracteres); 
+        $Hash .= substr($Caracteres,$Posicao,1); 
+    } 
+
+return $Hash; 
+} 
+
+//Here you specify how many characters the returning string must have 
+
+?>  
